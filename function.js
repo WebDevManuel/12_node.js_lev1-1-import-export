83b7d43b9bc49a6b6f3ab104d99baca142224787
WebDevManuel <donmanuel2005@web.de>
2023-05-10 14:35:27 +0200

    ✨ Add solution to the task

---
+++ function.js
@@ -0,0 +1,7 @@
+export const sortArrays = (arr) => {
+    
+    arr.sort()
+    return (
+        console.log(arr)
+    );
+};
---
+++ main.js
@@ -0,0 +1,5 @@
+import { arrayNumbers, arrayCars } from './data.js';
+import {sortArrays} from './function.js';
+
+sortArrays(arrayCars);
+sortArrays(arrayNumbers);
